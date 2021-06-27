a0eae38d90c55f9bd588c0f3c4b01cf97219fb63
Cyril Gourgouillon <cyrilgour@gmail.com>
2021-06-27 17:37:21 +0200

    add design and api

--- src/Components/QuoteBox.jsx
+++ src/Components/QuoteBox.jsx
@@ -1,12 +1,38 @@
-import React from 'react';
+import axios from 'axios';
+import React, {useState, useEffect} from 'react';
 
-const QuoteBox = () => (
-  <div id="quote-box">
-    <div id="text">text</div>
-    <div id="author">author</div>
-    <button type="button" id="new-quote">New quote</button>
-    <a id="tweet-quote" href="https://www.twitter.com/intent/tweet" target="_blank" rel="noreferrer">Tweet quote</a>
-  </div>
-);
+const QuoteBox = () => {
+  
+  const [quote, setQuote] = useState("En chargement...");
+  const [author, setAuthor] = useState("En chargement...");
+  
+  const apiLoadQuote = () => {
+    axios.get("https://api.quotable.io/random")
+          .then(r => {
+            setQuote(r.data.content)
+            setAuthor(r.data.author)
+          })
+          .catch(e => console.log(e));
+  }
+
+  const handleNewQuoteClick = () => {
+    apiLoadQuote();
+  };
+  
+  useEffect(() => {
+    apiLoadQuote();
+  }, []);
+
+  return (
+    <div id="main-container" className=".d-flex align-items-center justify-content-center">
+      <div id="quote-box">
+        <div id="text"><i class="fas fa-quote-left"></i><span class="quote-text"> {quote} </span> <i class="fas fa-quote-right"></i></div>
+        <div id="author"><i class="fas fa-angle-right"></i> {author}</div>
+        <button type="button" id="new-quote" className="btn btn-dark" onClick={handleNewQuoteClick}><i class="fas fa-sync-alt"></i></button>
+        <a id="tweet-quote" href={`https://www.twitter.com/intent/tweet?text=${quote} from ${author}`} target="_blank" rel="noreferrer"><i class="fab fa-twitter-square"></i></a>
+      </div>
+    </div>
+  );
+}
 
 export default QuoteBox;
